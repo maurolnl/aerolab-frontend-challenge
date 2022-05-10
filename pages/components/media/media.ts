bf0c25a0d556ca353bce9e379d37b1eaea9143c7
maurolnl <mauroleoquiroga@gmail.com>
2022-05-10 17:24:08 -0300

    Update media, add mobileS size

--- pages/components/media/media.ts
+++ pages/components/media/media.ts
@@ -1,9 +1,11 @@
 export const size = {
+  mobileS: "620px",
   mobile: "1023px",
   desktop: "1470px",
 };
 
 export const device = {
+  mobileS: `(max-width: ${size.mobileS})`,
   mobile: `(max-width: ${size.mobile})`,
   tablet: `(min-width: ${size.mobile}) and (max-width: ${size.desktop})`,
   desktop: `(min-width: ${size.desktop})`,
