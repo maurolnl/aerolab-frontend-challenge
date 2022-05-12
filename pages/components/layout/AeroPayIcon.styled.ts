c720113da6af40f8557099b8fb001d0d9516ff8c
maurolnl <mauroleoquiroga@gmail.com>
2022-05-12 16:32:14 -0300

    Update icon add isDisabled prop

--- pages/components/layout/AeroPayIcon.styled.ts
+++ pages/components/layout/AeroPayIcon.styled.ts
@@ -1,14 +1,26 @@
 import styled from "styled-components";
 import SVG from "react-inlinesvg";
 
+import {Colors} from "../../../styles/Theme";
+
 interface Props {
   variant: string;
   valign?: string;
+  isDisabled?: string;
 }
 
 export const Icon = styled(SVG)<Props>`
   vertical-align: ${(p) => (p.valign ? p.valign : "")};
 
+  > path {
+    fill: ${(p) => (p.isDisabled ? Colors.Neutral[0] : "")};
+  }
+
+  > rect {
+    fill: ${(p) => (p.isDisabled ? Colors.Neutral[500] : "")};
+  }
+
+  //Since aerocoins use this icon but its size on mobile small screens is still 24 x 24
   ${(p) => {
     switch (p.variant) {
       case "Desktop":
@@ -25,6 +37,8 @@ export const Icon = styled(SVG)<Props>`
         return `
           width: 20px;
           height: 20px;
+
+          vertical-align: sub;
       `;
     }
   }}
--- pages/components/layout/PagerIcon.styled.ts
+++ pages/components/layout/PagerIcon.styled.ts
@@ -1,13 +1,20 @@
 import styled from "styled-components";
 import SVG from "react-inlinesvg";
 
+import {Colors} from "../../../styles/Theme";
+
 interface Props {
   variant: string;
   rotation: string;
+  isDisabled?: boolean;
 }
 
 export const PagerIcon = styled(SVG)<Props>`
   transform: rotate(${(props) => props.rotation});
+
+  > path {
+    stroke: ${(p) => (p.isDisabled ? Colors.Neutral[300] : "")};
+  }
 
   ${(p) => {
     switch (p.variant) {
