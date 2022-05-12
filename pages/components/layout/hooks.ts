195c01bb365a6669e116ba21be15e48c0c40d62a
maurolnl <mauroleoquiroga@gmail.com>
2022-05-12 14:49:42 -0300

    Update #useMedia hook, returns true if ssr

--- pages/components/layout/hooks.ts
+++ pages/components/layout/hooks.ts
@@ -2,13 +2,17 @@ import {useEffect, useState} from "react";
 
 const useMedia = (queries: string[], values: boolean[], defaultValue = false) => {
   const mediaQueryLists: any[] = queries.map((q) =>
-    typeof window !== "undefined" ? window.matchMedia(q) : {},
+    typeof window !== "undefined" ? window.matchMedia(q) : false,
   );
 
   const getValue = () => {
-    const index = mediaQueryLists.findIndex((mql) => mql.matches);
+    if (mediaQueryLists[0]) {
+      const index = mediaQueryLists.findIndex((mql) => mql.matches);
 
-    return typeof values[index] !== "undefined" ? values[index] : defaultValue;
+      return index > -1 ? values[index] : defaultValue;
+    }
+
+    return true;
   };
 
   const [value, setValue] = useState<boolean>(getValue);
