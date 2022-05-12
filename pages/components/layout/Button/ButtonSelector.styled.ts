238c66201500aec6db1fab5db2486fe2add13633
maurolnl <mauroleoquiroga@gmail.com>
2022-05-12 16:37:59 -0300

    Update button mobile responsiveness

--- pages/components/layout/Button/ButtonSelector.styled.ts
+++ pages/components/layout/Button/ButtonSelector.styled.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 import {Colors, TextStyles} from "../../../../styles/Theme";
+import {device} from "../../media/media";
 
 interface Props {
   isActive: boolean;
@@ -10,20 +11,33 @@ interface Props {
 }
 
 export const ButtonSelector = styled.button<Props>`
-  ${TextStyles.Bundler(TextStyles.Texts.L1.Default)};
-  color: ${Colors.Neutral[100]};
-  background: ${(p) => (p.isActive ? Colors.Brand.Default.Color : Colors.Neutral[200])};
-  border-radius: 12px;
-  padding: ${(p) => (p.padding ? p.padding : "8px 0px")};
-  cursor: pointer;
-  border: none;
   display: flex;
   align-content: center;
   align-items: center;
   justify-content: center;
-  margin-bottom: 5px;
+
   width: ${(p) => (p.w ? p.w : "")};
   height: ${(p) => (p.h ? p.h : "")};
+
+  border: none;
+  border-radius: 12px;
+
+  margin-bottom: 5px;
+  padding: ${(p) => (p.padding ? p.padding : "8px 0px")};
+
+  color: ${Colors.Neutral[100]};
+  background: ${(p) => (p.isActive ? Colors.Brand.Default.Color : Colors.Neutral[200])};
+
+  cursor: pointer;
+  white-space: nowrap;
+
+  ${TextStyles.Bundler(TextStyles.Texts.L1.Default)};
+
+  @media ${device.mobile} {
+    ${TextStyles.Bundler(TextStyles.Texts.Mobile.L1.Default)};
+
+    padding: ${(p) => (p.padding ? "8px 16px" : "8px 0px")};
+  }
 
   &:hover {
     background: ${Colors.Brand.Hover.Color};
