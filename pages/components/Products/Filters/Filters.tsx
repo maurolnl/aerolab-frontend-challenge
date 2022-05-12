1cd8808cc65edab8de57b9885a95eb0f71836029
maurolnl <mauroleoquiroga@gmail.com>
2022-05-12 16:30:51 -0300

    Update pager display prop when resizing

--- pages/components/Products/Filters/Filters.tsx
+++ pages/components/Products/Filters/Filters.tsx
@@ -7,6 +7,7 @@ import {Divider} from "../../layout/Divider.styled";
 import useMedia from "../../layout/hooks";
 import {Stack} from "../../layout/Stack.styled";
 import ToggleGroup from "../../layout/ToggleGroup";
+import {device} from "../../media/media";
 
 import CategoryFilter from "./CategoryFilter";
 import Pager from "./Pager";
@@ -30,10 +31,15 @@ const CategoryAndSortWrapper = styled.div`
   padding: 0px;
 
   gap: 40px;
+
+  @media ${device.mobileS} {
+    width: 100%;
+  }
 `;
 
 const Filters = () => {
   const isDesktop = useMedia(["(min-width: 1470px)"], [true]);
+  const isMobile = useMedia(["(max-width: 1023px)"], [true]);
   const sortTypes = ["Most Recent", "Lowest Price", "Highest Price"];
 
   return (
@@ -45,7 +51,7 @@ const Filters = () => {
             <Divider display={isDesktop ? "" : "none"} src={Divider_115.src} variant="Desktop" />
             <Sort />
           </CategoryAndSortWrapper>
-          <Pager display="none" />
+          <Pager display={isMobile ? "none" : "flex"} />
         </Wrapper>
         <ToggleGroup
           display={isDesktop ? "none" : "flex"}
