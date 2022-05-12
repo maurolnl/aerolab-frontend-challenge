e4b87604043374444a23e23684dbbffd84133f67
maurolnl <mauroleoquiroga@gmail.com>
2022-05-12 16:34:41 -0300

    Update button, add isDisabled prop

--- pages/components/layout/Button/ButtonCTA.styled.ts
+++ pages/components/layout/Button/ButtonCTA.styled.ts
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import {Colors, Shadows, TextStyles} from "../../../../styles/Theme";
+import {device} from "../../media/media";
 
 interface Props {
   mt?: string;
@@ -13,24 +14,48 @@ interface Props {
 }
 
 export const ButtonCTA = styled.button<Props>`
-  color: ${Colors.Neutral[0]};
-  background: ${Colors.Brand.Default.Color};
-  width: ${(p) => (p.w ? p.w : "318px")};
-  height: ${(p) => (p.h ? p.h : "80px")};
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  margin-top: ${(p) => (p.mt ? p.mt : "")};
-  cursor: pointer;
-  border-radius: ${(p) => (p.borderRadius ? p.borderRadius : "24px")};
+
+  width: ${(p) => (p.w ? p.w : "318px")};
+  height: ${(p) => (p.h ? p.h : "80px")};
+
   border: none;
+  border-radius: ${(p) => (p.borderRadius ? p.borderRadius : "24px")};
+
   padding: 0px;
+  margin-top: ${(p) => (p.mt ? p.mt : "")};
+
+  color: ${Colors.Neutral[0]};
+  background: ${Colors.Brand.Default.Color};
   box-shadow: ${Shadows.Elevation1.Default};
 
+  cursor: pointer;
+
   &:hover {
-    background: ${Colors.Brand.Hover.Color};
+    background: ${(p) =>
+      p.variant === "Disabled" ? Colors.Neutral[200] : Colors.Brand.Hover.Color};
   }
+
+  ${(p) => {
+    switch (p.variant) {
+      case "Processing":
+        return `opacity: 70%;`;
+
+      case "Disabled":
+        return `
+          background: ${Colors.Neutral[200]};
+          color: ${Colors.Neutral[600]};
+
+          cursor: not-allowed;
+        `;
+
+      default:
+        break;
+    }
+  }}
 
   ${(p) => {
     switch (p.textVariant) {
@@ -41,4 +66,16 @@ export const ButtonCTA = styled.button<Props>`
         return TextStyles.Bundler(TextStyles.Texts.L1.Default);
     }
   }}
+
+  @media ${device.mobileS} {
+    ${(p) => {
+      switch (p.textVariant) {
+        case "AllCaps":
+          return TextStyles.Bundler(TextStyles.Texts.Mobile.L1.AllCapsUnspaced);
+
+        default:
+          return TextStyles.Bundler(TextStyles.Texts.Mobile.L1.Default);
+      }
+    }}
+  }
 `;
