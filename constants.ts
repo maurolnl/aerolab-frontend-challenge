27beb76fecc6e350522b82ec537c7a2dac1d6012
maurolnl <mauroleoquiroga@gmail.com>
2022-05-16 19:36:11 -0300

    Add constants.ts

---
+++ constants.ts
@@ -0,0 +1,18 @@
+export const PRODUCT_QUANTITY = 32;
+export const SORT_TYPES = ["Most Recent", "Lowest Price", "Highest Price"];
+export const PRODUCTS_CATEGORIES = [
+  "All Products",
+  "Phones",
+  "Tablets & E-readers",
+  "Phone Accessories",
+  "PC Accessories",
+  "Gaming",
+  "Laptops",
+  "Cameras",
+  "Audio",
+  "Smart Home",
+  "Monitors & TV",
+  "Drones",
+];
+export const PROD_URL = "https://coding-challenge-api.aerolab.co/";
+export const MOCK_URL = "https://private-anon-c695ebc5f5-aerolabchallenge.apiary-mock.com/";
