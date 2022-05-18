402b9b6a925d5990e17a84ce824fadca21c09054
maurolnl <mauroleoquiroga@gmail.com>
2022-05-18 20:00:02 -0300

    Update hooks, add #usePager and #usePageLimit

--- components/layout/hooks.ts
+++ components/layout/hooks.ts
@@ -1,4 +1,7 @@
+import {useRouter} from "next/router";
 import {useEffect, useState} from "react";
+
+import {PRODUCT_QUANTITY} from "../../constants";
 
 export const useMedia = (queries: string[], values: boolean[], defaultValue = false) => {
   const mediaQueryLists: any[] = queries.map((q) =>
@@ -26,4 +29,61 @@ export const useMedia = (queries: string[], values: boolean[], defaultValue = fa
   }, []);
 
   return value;
+};
+
+export const usePager = () => {
+  const [pageNumber, setPageNumber] = useState<number>(1);
+
+  const router = useRouter();
+  const {page} = router.query;
+
+  useEffect(() => {
+    if (page) {
+      setPageNumber(Number(page));
+    }
+  }, [page]);
+
+  const handleNextPage = () => {
+    router.push(`/?page=${pageNumber + 1}`, undefined, {scroll: false});
+  };
+
+  const handlePreviousPage = () => {
+    router.push(`/?page=${pageNumber - 1}`, undefined, {scroll: false});
+  };
+
+  const handleNavigate = (page: number) => {
+    router.push(`/?page=${page}`, undefined, {scroll: false});
+  };
+
+  return {
+    page: pageNumber,
+    handleNextPage,
+    handlePreviousPage,
+    handleNavigate,
+  };
+};
+
+export const usePageLimit = () => {
+  const [limitPerPage, setLimit] = useState<number>(16);
+  const isDesktop = useMedia(["(min-width: 1470px)"], [true]);
+  const isMobile = useMedia(["(max-width: 1023px)"], [true]);
+  const isMobileS = useMedia(["(max-width: 620px)"], [true]);
+  const isTablet = !isDesktop && !isMobile && !isMobileS;
+
+  useEffect(() => {
+    if (isTablet) {
+      setLimit(12);
+
+      return;
+    } else if (isMobile) {
+      setLimit(8);
+
+      return;
+    }
+    setLimit(16);
+  }, [isDesktop, isMobile, isTablet]);
+
+  return {
+    limit: limitPerPage,
+  };
 };
